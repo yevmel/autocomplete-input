11726e08ed4c549004b993153c8f8b3401df6645
Yevgeniy Melnichuk <yevgeniy.melnichuk@googlemail.com>
2024-01-29 23:33:47 +0100

    handle all kind of values (not just addresses from sample data)

--- addresses-app/src/main/javascript/script.js
+++ addresses-app/src/main/javascript/script.js
@@ -1,3 +1,5 @@
+const CurrentSuggestions = new WeakMap()
+
 /**
  * executes <b>eventHandlerFn</b> only if there is an ancestor to the target matching <b>closestElementSelector</b>
  *
@@ -36,30 +38,26 @@ function restoreCurrentText(target) {
    }
 }
 
-function applySuggestion(autocompleteContainerEl, suggestionEl) {
-   autocompleteContainerEl.querySelector('.autocomplete-value').value =
-      suggestionEl.dataset.autocompleteSuggestion
+function applySuggestion(autocompleteContainerEl, suggestionEl, valueMapper, displayMapper) {
+   const suggestionIdx = Array.from(
+      autocompleteContainerEl.querySelectorAll('.suggestion')
+   ).indexOf(suggestionEl)
+
+   const suggestionsEl = autocompleteContainerEl.querySelector('.suggestions')
+   const values = CurrentSuggestions.get(suggestionsEl)[suggestionIdx]
+
+   const autocompleteValueEl = autocompleteContainerEl.querySelector('.autocomplete-value')
+   autocompleteValueEl.value = valueMapper(values)
 
    const autocompleteTextEl = autocompleteContainerEl.querySelector('.autocomplete-text')
-   autocompleteTextEl.value = autocompleteTextEl.dataset.autocompleteCurrentText =
-      suggestionEl.dataset.autocompleteSuggestionText
-}
-
-async function setupSuggestionsHtml(response) {
-   const contentType = response.headers.get("Content-Type")
-
-   if ('application/json' === contentType) {
-      const addresses = await response.json();
-      return setupSuggestionsHtmlFromJson(addresses)
-   }
-
-   throw Error('can not handle content type: ' + contentType)
+   autocompleteTextEl.value = autocompleteTextEl.dataset.autocompleteCurrentText = displayMapper(values)
 }
 
 function setupSuggestionsHtmlFromJson(addresses) {
    const rows = addresses.length === 0 ? `<tr><td colspan="999">no suggestions for you ¯\\_(ツ)_/¯</td></tr>` : addresses.map(address => `
-      <tr data-autocomplete-suggestion="${address.id}" 
-          data-autocomplete-suggestion-text="${address.firstname} ${address.lastname}, ${address.city}, ${address.street}">
+      <tr class="suggestion" 
+        data-autocomplete-suggestion="${address.id}" 
+        data-autocomplete-suggestion-text="${address.firstname} ${address.lastname}, ${address.city}, ${address.street}">
          <td>${address.firstname}</td>
          <td>${address.lastname}</td>
          <td>${address.city}, ${address.street}</td>
@@ -85,12 +83,23 @@ function setupSuggestionsHtmlFromJson(addresses) {
    `
 }
 
-(function setupAutocomplete(
-   root = document
-) {
+function removeSuggestions(suggestionsEl) {
+   const deleted = CurrentSuggestions.delete(suggestionsEl)
+   if (!deleted) {
+      console.warn("failed to cleanup suggestion values.")
+   }
+
+   suggestionsEl.remove()
+}
+
+function setupAutocomplete({
+   root = document,
+   valueMapper = values => values.id,
+   displayMapper = values => values.id
+} = {}) {
    let abortController
 
-   root.addEventListener("focusout", withClosestElement(".autocomplete", function(event, autocompleteContainer) {
+   root.addEventListener("focusout", withClosestElement(".autocomplete", function(event, autocompleteContainerEl) {
       if (abortController) {
          abortController.abort()
          abortController = null
@@ -98,27 +107,27 @@ function setupSuggestionsHtmlFromJson(addresses) {
 
       restoreCurrentText(event.target)
 
-      const suggestions = autocompleteContainer.querySelector('.suggestions')
-      if (suggestions) {
-         suggestions.remove()
+      const suggestionsEl = autocompleteContainerEl.querySelector('.suggestions')
+      if (suggestionsEl) {
+         removeSuggestions(suggestionsEl)
       }
    }))
 
-   root.addEventListener("mousedown", withClosestElement("[data-autocomplete-suggestion]", function(event, suggestion) {
-      const autocompleteContainer = suggestion.closest('.autocomplete')
-      applySuggestion(autocompleteContainer, suggestion)
+   root.addEventListener("mousedown", withClosestElement(".suggestion", function(event, suggestionEl) {
+      const autocompleteContainerEl = suggestionEl.closest('.autocomplete')
+      applySuggestion(autocompleteContainerEl, suggestionEl, valueMapper, displayMapper)
    }))
 
    root.addEventListener('keydown', withClosestElement(".autocomplete", function(event, autocompleteContainerEl) {
-      const suggestionsContainer = autocompleteContainerEl.querySelector('.suggestions')
-      if (!suggestionsContainer) {
+      const suggestionsContainerEl = autocompleteContainerEl.querySelector('.suggestions')
+      if (!suggestionsContainerEl) {
          return
       }
 
       if (["ArrowDown", "ArrowUp"].indexOf(event.key) > -1) {
          event.preventDefault()
 
-         const suggestions = Array.from(suggestionsContainer.querySelectorAll('[data-autocomplete-suggestion]'))
+         const suggestions = Array.from(suggestionsContainerEl.querySelectorAll('[data-autocomplete-suggestion]'))
          const selectedIdx = suggestions.findIndex(el => el.matches('.selected'))
 
          if ("ArrowDown" === event.key) {
@@ -137,13 +146,13 @@ function setupSuggestionsHtmlFromJson(addresses) {
             }
          }
       } else if ("Escape" === event.key) {
-         suggestionsContainer.remove()
+         removeSuggestions(suggestionsContainerEl)
          restoreCurrentText(event.target)
       } else if ("Enter" === event.key) {
-         const selectedSuggestionEl = suggestionsContainer.querySelector('.selected')
+         const selectedSuggestionEl = suggestionsContainerEl.querySelector('.selected')
          if (selectedSuggestionEl) {
-            applySuggestion(autocompleteContainerEl, selectedSuggestionEl)
-            suggestionsContainer.remove()
+            applySuggestion(autocompleteContainerEl, selectedSuggestionEl, valueMapper, displayMapper)
+            removeSuggestions(suggestionsContainerEl)
          }
       }
    }))
@@ -169,18 +178,32 @@ function setupSuggestionsHtmlFromJson(addresses) {
          headers: { "Accept": "application/json" },
          signal: abortController.signal
       }).then(async response => {
-         const html = await setupSuggestionsHtml(response)
-         const autocompleteContainer = event.target.closest('.autocomplete')
+         const contentType = response.headers.get("Content-Type")
 
-         const existingSuggestions = autocompleteContainer.querySelector('.suggestions')
-         if (existingSuggestions) {
-            existingSuggestions.remove()
+         if ('application/json' === contentType) {
+            const addresses = await response.json();
+            const html = setupSuggestionsHtmlFromJson(addresses)
+            const autocompleteContainerEl = event.target.closest('.autocomplete')
+
+            const existingSuggestionsEl = autocompleteContainerEl.querySelector('.suggestions')
+            if (existingSuggestionsEl) {
+               removeSuggestions(existingSuggestionsEl)
+            }
+
+            const tempEl = document.createElement("div");
+            tempEl.innerHTML = html
+
+            const newSuggestionsEl = tempEl.firstElementChild;
+            CurrentSuggestions.set(newSuggestionsEl, addresses)
+
+            autocompleteContainerEl.appendChild(newSuggestionsEl)
+         } else {
+            throw Error('can not handle content type: ' + contentType)
          }
-
-         const temp = document.createElement("div");
-         temp.innerHTML = html
-
-         autocompleteContainer.appendChild(temp.firstElementChild)
       })
    }))
-})()
+}
+
+setupAutocomplete({
+   displayMapper: values => `${values.firstname} ${values.lastname}, ${values.city}, ${values.street}`
+})
